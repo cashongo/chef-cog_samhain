1839161f7cdcb716b37e1f44161df3b7e8117dbc
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-11-06 16:43:25 +0200

    Version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cog_samhain'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Cash on Go Ltd'
+maintainer_email 'lauri.jesmin@cashongo.co.uk'
 license          'All rights reserved'
 description      'Installs/Configures cog_samhain'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
