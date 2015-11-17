b7dc08904429dfcf02933fa23a7673da3f81bdfe
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-11-17 10:51:29 +0200

    move packages to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,8 @@
 default['cog_samhain']['samhain_version']  = '4.1.0'
+
+case node['platform_family']
+when 'rhel'
+  default['cog_samhain']['packages']  = ['gcc','make','libattr-devel','libacl-devel','pcre-devel','audit-libs-devel','zlib-devel']
+when 'debian'
+  default['cog_samhain']['packages'] =  ['libaudit-dev','gcc','make','libattr1-dev','libacl1-dev','libpcre3-dev','zlib1g-dev']
+end
